0101fe2ae79f3f90fecc43d903668f558eaa6e34
GuilhermeSperfeldVorgnes <guilhermesperfeldvorgnes@gmail.com>
2025-05-15 16:50:36 -0400

    HFGH

--- exercicio13.js
+++ exercicio13.js
@@ -1,47 +1,15 @@
-let readline = require('readline');
+let dia = 7;
 
-let rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
+if (dia == 1) console.log("Domingo");
 
+if (dia == 2) console.log("Segunda");
 
-rl.question('Digite um número de 1 a 7 para se referir um dia da semana: ', (numeroDia) => {
-    let dia = perseInt(numeroDia);
+if (dia == 3) console.log("Terça");
 
-switch (dia) {
-        case 1:
-            console.log("Segunda");
-            break;
-        
-        case 2:
-            console.log("Terça");
-            break;
-        
-        case 3:
-            console.log("Quarta");
-            break;
-        
-        case 4:
-            console.log("Quinta");
-            break;
-    
-        case 5:
-            console.log("Sexta");
-            break;   
-    
-        case 6:
-            console.log("Sabado");
-            break;
-            
-        case 7:
-            console.log("Domingo");
-            break;
-            
-        default:
-            console.log("Não é um numero da semana");
-            break;
-    }
+if (dia == 4) console.log("Quarta");
 
-    rl.close();
-});
+if (dia == 5) console.log("Quinta");
+
+if (dia == 6) console.log("Sexta");
+
+if (dia == 7) console.log("Sabado");
---
+++ exercicio14.js
@@ -0,0 +1,25 @@
+let mes = 7;
+
+if (mes == 1) console.log("Janeiro");
+
+if (mes == 2) console.log("Fevereiro");
+
+if (mes == 3) console.log("Março");
+
+if (mes == 4) console.log("Abril");
+
+if (mes == 5) console.log("Maio");
+
+if (mes == 6) console.log("Junho");
+
+if (mes == 7) console.log("Julho");
+
+if (mes == 8) console.log("Agosto");
+
+if (mes == 9) console.log("Setembro");
+
+if (mes == 10) console.log("Outubro");
+
+if (mes == 11) console.log("Novembro");
+
+if (mes == 12) console.log("Dezembro");
