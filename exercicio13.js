dc4efcd74ff6a995718447c5b7a7d1b8828d6584
GuilhermeSperfeldVorgnes <guilhermesperfeldvorgnes@gmail.com>
2025-05-15 17:08:39 -0400

    mbch

--- exercicio13.js
+++ exercicio13.js
@@ -13,3 +13,44 @@ if (dia == 5) console.log("Quinta");
 if (dia == 6) console.log("Sexta");
 
 if (dia == 7) console.log("Sabado");
+
+/*
+let readline = require('readline');
+
+let rl = readline.createInterface({
+input: process.stdin,
+output: process.stdout
+});
+
+rl.question('Digite um número de 1 a 7 para representar um dia da semana: ', (numeroDia) => {
+let dia = parseInt(numeroDia);
+
+// Switch case (como um if/else)
+switch(dia) {
+case 1:
+console.log("Domingo");
+break;
+case 2:
+console.log("Segunda-feira");
+break;
+case 3:
+console.log("Terça-feira");
+break;
+case 4:
+console.log("Quarta-feira");
+break;
+case 5:
+console.log("Quinta-feira");
+break;
+case 6:
+console.log("Sexta-feira");
+break;
+case 7:
+console.log("Sábado");
+break;
+default:
+console.log("Número inválido! Digite um número de 1 a 7.");
+}
+
+rl.close();
+});*/
--- exercicio14.js
+++ exercicio14.js
@@ -23,3 +23,60 @@ if (mes == 10) console.log("Outubro");
 if (mes == 11) console.log("Novembro");
 
 if (mes == 12) console.log("Dezembro");
+
+/*
+let readline = require('readline');
+
+let rl = readline.createInterface({
+input: process.stdin,
+output: process.stdout
+});
+
+rl.question('Digite um número de 1 a 12 para representar um mês: ', (numeroMes) => {
+let mes = parseInt(numeroMes);
+
+switch(mes) {
+case 1:
+console.log("Janeiro");
+break;
+case 2:
+console.log("Fevereiro");
+break;
+case 3:
+console.log("Março");
+break;
+case 4:
+console.log("Abril");
+break;
+case 5:
+console.log("Maio");
+break;
+case 6:
+console.log("Junho");
+break;
+case 7:
+console.log("Julho");
+break;
+case 8:
+console.log("Agosto");
+break;
+case 9:
+console.log("Setembro");
+break;
+case 10:
+console.log("Outubro");
+break;
+case 11:
+console.log("Novembro");
+break;
+case 12:
+console.log("Dezembro");
+break;
+default:
+console.log("Número inválido! Digite um número de 1 a 12.");
+}
+
+rl.close();
+});
+
+*/
