10deaaa29bc9fc0b4fc0024c142184d48853dde7
GuilhermeSperfeldVorgnes <guilhermesperfeldvorgnes@gmail.com>
2025-07-10 17:36:50 -0400

    j,xf

--- tarefas 2Bimestra/index.js
+++ tarefas 2Bimestra/index.js
@@ -1,11 +1,23 @@
-const express = require('express');
+import express from 'express';
 const app = express();
+import { efetuarCalculoIMC , retornaStatusIMC, validaParametro} from './servicos/calculadoraIMC.js';
 
 app.get('/',(req, res) => {
-    res.send('Hello World!');
-})
+    let peso = req.query.peso;
+    let altura = req.query.altura;
+    if(validaParametro(peso) && validaParametro(altura)){
+
+        let imc = efetuarCalculoIMC(peso, altura);
+        let status = retornaStatusIMC(imc);
+    
+        res.json({imc: imc, status: status})
+    }else{
+        res.status(400).json({Erro: 'Peso e/ou altura inválidos!'})
+    }
+});
+
 
 app.listen(8080, () => {
     let data = new Date();
     console.log("Servidor node iniciado em: " + data);
-})
+});
---
+++ tarefas 2Bimestra/servicos/calculadoraIMC.js
@@ -0,0 +1,34 @@
+function efetuarCalculoIMC(peso, altura) {
+    let imc = peso / (altura * altura);
+    return imc.toFixed(2);
+}
+
+function retornaStatusIMC(imc) {
+    let status;
+    if (imc < 18.5) {
+        status = 'Abaixo do peso';
+    } else if (imc < 24.9) {
+        status = 'Peso normal';
+    } else if (imc < 30) {
+        status = 'Acima do peso';
+    } else {
+        status = 'Obeso';
+    }
+    return status;
+}
+
+function validaParametro(parametro) {
+    if(parametro){
+
+        if (isNaN(parametro)) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+    else{
+        return false;
+    }
+}
+
+export {efetuarCalculoIMC, retornaStatusIMC, validaParametro};
